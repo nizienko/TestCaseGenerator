b375c3db59eac566f776c761685de2c140c06a17
nizienko <nizienko@outlook.com>
2014-07-13 21:30:54 +0400

    Initial commit

--- src/main/java/ru/yandex/money/def/Gui.java
+++ src/main/java/ru/yandex/money/def/Gui.java
@@ -13,6 +13,8 @@ import java.io.File;
 public class Gui extends JFrame{
     private JMenuBar menuBar;
     private JMenu menuModel;
+    private JMenuItem openTxt;
+    private JMenuItem saveTxt;
     private JMenuItem generateCases;
     private JPanel mainPanel;
     private JTextArea textArea;
@@ -27,15 +29,24 @@ public class Gui extends JFrame{
 
 
         menuBar = new JMenuBar();
-        menuModel = new JMenu("Model");
+        menuModel = new JMenu("File");
         menuModel.setMnemonic(KeyEvent.VK_M);
-        menuModel.getAccessibleContext().setAccessibleDescription("Model");
+        menuModel.getAccessibleContext().setAccessibleDescription("File");
         menuBar.add(menuModel);
+        openTxt = new JMenuItem("Open", KeyEvent.VK_O);
+        openTxt.addActionListener(listener);
+        openTxt.setActionCommand("open");
+        menuModel.add(openTxt);
+        saveTxt = new JMenuItem("Save", KeyEvent.VK_O);
+        saveTxt.addActionListener(listener);
+        saveTxt.setActionCommand("save");
+        menuModel.add(saveTxt);
         generateCases = new JMenuItem("Export to freeMind", KeyEvent.VK_E);
         menuModel.add(generateCases);
         textArea = new JTextArea(text);
         mainPanel.add(textArea);
         generateCases.addActionListener(listener);
+        generateCases.setActionCommand("export");
         this.setJMenuBar(menuBar);
 
         this.setContentPane(mainPanel);
@@ -53,12 +64,44 @@ public class Gui extends JFrame{
         return textArea.getText();
     }
 
+    public void setModel(String txt) {
+        this.textArea.setText(txt);
+    }
+
     public File getSrcFile(String filename){
         dialog = new JFileChooser();
         dialog.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
         dialog.setApproveButtonText("Сохранить");
         dialog.setDialogTitle("Куда сохранить");
         dialog.setSelectedFile(new File(filename));
+        dialog.setDialogType(JFileChooser.SAVE_DIALOG);
+        dialog.setMultiSelectionEnabled(false);
+        int res = dialog.showOpenDialog(this);
+        if (res == JFileChooser.APPROVE_OPTION) {
+            return dialog.getSelectedFile();
+        }
+        return null;
+    }
+
+    public File openTxtFile(){
+        dialog = new JFileChooser();
+        dialog.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        dialog.setApproveButtonText("Открыть");
+        dialog.setDialogTitle("Открыть текстовый файл");
+        dialog.setDialogType(JFileChooser.OPEN_DIALOG);
+        dialog.setMultiSelectionEnabled(false);
+        int res = dialog.showOpenDialog(this);
+        if (res == JFileChooser.APPROVE_OPTION) {
+            return dialog.getSelectedFile();
+        }
+        return null;
+    }
+
+    public File saveTxtFile(){
+        dialog = new JFileChooser();
+        dialog.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        dialog.setApproveButtonText("Сохранить");
+        dialog.setDialogTitle("Куда сохранить");
         dialog.setDialogType(JFileChooser.SAVE_DIALOG);
         dialog.setMultiSelectionEnabled(false);
         int res = dialog.showOpenDialog(this);
